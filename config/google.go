f75c4356b0a9a2cadf719dea72ba8f8c3a28a241
Jhonatan Morais <jhonatanvinicius@gmail.com>
2026-07-17 08:39:22 +0000

    fix(logger): Expose attributes to envConfig

--- config/google.go
+++ config/google.go
@@ -2,22 +2,7 @@ package config
 
 // GoogleConfig holds the configuration for Google OAuth2.
 type GoogleConfig struct {
-	clientID     string `envconfig:"GOOGLE_CLIENT_ID"`
-	clientSecret string `envconfig:"GOOGLE_CLIENT_SECRET"`
-	redirectURL  string `envconfig:"AUTH_REDIRECT_URL"`
-}
-
-// ClientID returns the Google client ID.
-func (g GoogleConfig) ClientID() string {
-	return g.clientID
-}
-
-// ClientSecret returns the Google client secret.
-func (g GoogleConfig) ClientSecret() string {
-	return g.clientSecret
-}
-
-// RedirectURL returns the Google redirect URL.
-func (g GoogleConfig) RedirectURL() string {
-	return g.redirectURL
+	ClientID     string `envconfig:"GOOGLE_CLIENT_ID"`
+	ClientSecret string `envconfig:"GOOGLE_CLIENT_SECRET"`
+	RedirectURL  string `envconfig:"AUTH_REDIRECT_URL"`
 }
--- config/openai.go
+++ config/openai.go
@@ -2,10 +2,5 @@ package config
 
 // OpenAIConfig holds the configuration for OpenAI API.
 type OpenAIConfig struct {
-	apiKey string `envconfig:"OPENAI_API_KEY"`
-}
-
-// APIKey returns the OpenAI API key.
-func (o OpenAIConfig) APIKey() string {
-	return o.apiKey
+	APIKey string `envconfig:"OPENAI_API_KEY"`
 }
